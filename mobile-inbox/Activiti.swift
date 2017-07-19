8c05f9c08b96037856635b3c4b48c25088124eb1
alexanderlesin <alexander.lesin@openiam.com>
2017-07-19 01:35:50 -0700

    add accept&reject

--- mobile-inbox/Activiti.swift
+++ mobile-inbox/Activiti.swift
@@ -148,7 +148,156 @@ class Activiti {
             task.createdTime = Date(timeIntervalSince1970: Double(createdTime) / 1000.0)
         }
         
+        if let endDate = json["endDate"].int {
+            task.endDate = Date(timeIntervalSince1970: Double(endDate) / 1000.0)
+        }
         
-        
+        task.processInstanceId = json["processInstanceId"].string;
+        task.processDefinitionId = json["processDefinitionId"].string;
+
+        return task;
+    }
+
+    private func parseTasks(_ json: JSON) -> [Task]? {
+        let isEmptyBean = json["emptySearchBean"].bool
+        guard let size = json["size"].int else { return nil }
+
+        if isEmptyBean == nil || isEmptyBean! || size <= 0 {
+            return nil
+        }
+
+        guard let beans = json["beans"].array else { return nil }
+
+        var tasks = [Task]()
+        for bean in beans {
+            if let task = parseBean(bean) {
+                tasks.append(task)
+            }
+        }
+        return tasks
+    }
+
+    // MARK: - Task Details
+
+    private func parseTaskDetails(_ json: JSON) -> TaskDetails? {
+        guard let id = json["id"].string else { return nil }
+        var details = TaskDetails(id)
+
+        details.name                        = json["name"].string
+        details.customObjectURI             = json["customObjectURI"].string
+        details.taskDefinitionKey           = json["taskDefinitionKey"].string
+        details.associationId               = json["associationId"].string
+        details.description                 = json["description"].string
+        details.executionId                 = json["executionId"].string
+        details.employeeId                  = json["employeeId"].string
+        details.workflowName                = json["workflowName"].string
+        details.associationType             = json["associationType"].string
+        details.processInstanceId           = json["processInstanceId"].string
+        details.processDefinitionId         = json["processDefinitionId"].string
+        details.owner                       = json["owner"].string
+        details.memberAssociationId         = json["memberAssociationId"].string
+        details.assignee                    = json["assignee"].string
+        details.attestationManagedSysFilter = json["attestationManagedSysFilter"].string
+        details.parentTaskId                = json["parentTaskId"].string
+        details.memberAssociationType       = json["memberAssociationType"].string
+        details.priority                    = json["priority"].int
+
+        if let dueDate = json["dueDate"].int {
+            details.dueDate = Date(timeIntervalSince1970: Double(dueDate) / 1000.0)
+        }
+
+        if let createdTime = json["createdTime"].int {
+            details.createdTime = Date(timeIntervalSince1970: Double(createdTime) / 1000.0)
+        }
+
+        if let endDate = json["endDate"].int {
+            details.endDate = Date(timeIntervalSince1970: Double(endDate) / 1000.0)
+        }
+
+        if let metadataMap = json["requestMetadataMap"].dictionary {
+            for metadata in metadataMap {
+                details.requestMetadataMap[metadata.key] = metadata.value.string
+            }
+        }
+
+        return details
+    }
+
+    func readTaskDetails(_ id: String, inBackground: Bool = true, completionHandler: @escaping (Activiti.TaskDetails?) -> Void) {
+        rest.apiCall(taskkDetailPath, 
+                     method: .get, 
+                     parameters: ["id": id],
+                     withRetryCount: 2,
+                     inBackground: inBackground) { response in
+                        if(response.json != nil) {
+                            let details = self.parseTaskDetails(response.json!)
+                            completionHandler(details)
+                        } else {
+                            completionHandler(nil)
+                        }
+
+                     }
+    }
+
+    // MARK: - Accept and Reject
+
+    private func parseDecisionResult(_ json: JSON) -> Bool {
+        guard let error = json["error"].bool else { return false }
+        if error { return false }
+
+        guard let status = json["status"].int else { return false }
+        if status != 200 { return false }
+
+        return true
+    }
+
+    func taskDecision(_ id: String,
+                        accepted: Bool,
+                        comment: String?
+                        completionHandler: @escaping (Bool?) -> Void) {
+
+        rest.apiCall(decisionPath,
+                     method: .post,
+                     parameters: ["taskId": id,
+                                  "accepted": accepted,
+                                  "comment": comment ?? ""]),
+                     withRetryCount: 2,
+                     inBackground: false) { response in
+                        if response.json != nil {
+                            let decisionResult = self.parseDecisionResult(response.json!)
+                            completionHandler(decisionResult)
+                        } else {
+                            completionHandler(nil)
+                        }
+
+        }
+    }
+
+    private func convertStrsToStr(_ strs: [String]) -> String? {
+        if strs.count == 0 { return "" }
+
+        var result = ""
+        for str in strs {
+            if !result.isEmpty {
+                result += ","
+            }
+            result += "\"\(str)\""
+        }
+        return "[\(result)]"
+    }
+
+    func tasksDecision(_ ids: [String], 
+                        accepted: Bool, 
+                        completionHandler: @escaping (Bool?) -> Void) {
+        rest.rawPostCall(accepted ? decisionListAcceptPath : decisionListRejectPath,
+                         body: convertStrsToStr(ids) ?? "",
+                         withRetryCount: 2) { response in
+                            if response.json != nil {
+                                let decisionResult = self.parseDecisionResult(response.json!)
+                                completionHandler(decisionResult)
+                            } else {
+                                completionHandler(nil)
+                            }
+                         }
     }
 }
